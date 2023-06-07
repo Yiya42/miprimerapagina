778848047788cae170b290e77de8367cd10a073b
Yiya42 <yiya1981@gmail.com>
2023-06-06 22:09:06 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function showAlert(){ alert('¡Hola, esta es una desde Javascript!');}
+document.querySelector("button.button-menu-toggle")
+     addEventListener("click",function(){
+                     document.querySelector(".nav-links").
+                     classList.toggle("nav-links-responsive")})
